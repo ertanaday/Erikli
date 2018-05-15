dc7ebf0e6e5e8abc8c9401e64bcabc0def5ffa99
arifbozdag <bozdag.arif@gmail.com>
2018-05-15 17:59:32 +0300

    fcfs corrected

--- dc.c
+++ dc.c
@@ -37,7 +37,6 @@ int main (int argc, char *argv[])
    for(int i = 0; i < 200; i++){
       arr[i].arrive_time = -1;
       arr[i].address = -1;
-      arr[i].wait_time = -1;
    }
    int i = 0; //number of requests
 
@@ -61,55 +60,90 @@ int main (int argc, char *argv[])
       printf("%d %d\n",arr[i].arrive_time, arr[i].address);
    }
 ////////////////////////////////////////////////////////////////////////
-   
-   sstf(arr, i);
+
+   //sstf(arr, i);
+   fcfs(arr);
 }
 
-void sstf( struct request arr[], int i )
+//FCFS
+void fcfs(struct request arr[]) {
+    printf("FCFS DISK SCHEDULING ALGORITHM:\n");
+    int i;
+    int totalTimeSpent = arr[0].arrive_time;
+    int currentPosition = 1;
+    for (i = 0; i < sizeof(arr); i++) {
+        //printf("%d, ", arr[i]);
+      if(arr[i].arrive_time <= totalTimeSpent){
+          arr[i].wait_time = totalTimeSpent - arr[i].arrive_time;
+          printf("wait_time: %d\n", arr[i].wait_time);
+          totalTimeSpent += abs(arr[i].address - currentPosition);
+          currentPosition = arr[i].address;
+    }
+      else{
+        i--;
+        totalTimeSpent++;
+      }
+
+    }
+    //printf("%d\n", arr[199]);
+    totalTimeSpent += abs(arr[i].address - currentPosition);
+    printf("FCFS: %d\n", totalTimeSpent);
+}
+
+//SSTF
+void sstf( struct request arr[], int size )
 {
    int head = 0; //initial disk head position
-   int closest_request = 0;
+   int closest_request = NULL;
    int time = arr[0].arrive_time; //initial time
-   int next_position = arr[0].address;
-   
-   //SSTF
+   printf("time = %d\n", time );
+   int next_position = NULL;//arr[0].address;
+   printf("next_position = %d\n", next_position );
 
    int k = 0;
-   while( i > k){
-
-      int j = 0;
-      int candidate_next_position;
+   while( k < size)
+   {
+      int candidate_next_position = next_position;
       bool empty_queue = true;
 
       //find closest request if available
-      while( arr[j].arrive_time != -1 && arr[j].arrive_time <= time)//while there is request in queue
+      int j = 0;
+      while( arr[j].arrive_time <= time )//while there is request in queue
       {
-         empty_queue = false;
-         candidate_next_position = arr[j].address;
-         if( abs( candidate_next_position - head ) < abs( next_position - head ) )
-         {
-            next_position = candidate_next_position;
-            closest_request = j;
-         }
-         j++;
-      }
+        if( arr[j].arrive_time != -1 )
+        {
+          empty_queue = false;
 
+          candidate_next_position = arr[j].address;
+          printf("candidate_next_position = %d\n", candidate_next_position );
+          if( abs( candidate_next_position - head ) < abs( next_position - head ) )
+          {
+             next_position = candidate_next_position;
+             closest_request = j;
+             printf("closest = %d\n", closest_request );
+          }
+        }
+        j++;
+      }
+      printf("next_position2 = %d\n", next_position );
+      printf("time2 = %d\n", time );
       //update head, time and array
       time += abs( next_position - head );
       head = next_position;
       arr[closest_request].arrive_time = -1;
+      k++;
+      printf("next_position2 = %d\n", next_position );
+      printf("time2 = %d\n", time );
+      for(int i = 0; i<10; i++)
+      {
+         printf("%d %d\n",arr[i].arrive_time, arr[i].address);
+      }
 
       //if no request is available increment time
       if(empty_queue)
       {
          time++;
       }
-      
-
-      k++;
    }
-   printf("%d\n", time);
+   printf("result = %d\n", time);
 }
-
-
-
